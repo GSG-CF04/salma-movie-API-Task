53eda7eec0f1659847b9dd30cea7f362091499f2
SalmaIssa96 <salmaissa196@gmail.com>
2021-11-20 22:50:35 +0200

    DOM-movie-description-section relates#8

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 let searchInput = document.querySelector(".search-input");
 let searchIcon = document.querySelector("#search-icon");
+const cutText = (text) => {
+  if (text.length > 550) {
+    let cutTextResult = text.slice(0, 551);
+    return cutTextResult + "...";
+  } else {
+    return text;
+  }
+};
 searchIcon.addEventListener("click", () => {
   let searchInputValue = searchInput.value;
   if (!searchInputValue) return;
-  fetch(`https://imdb-api.com/en/API/Search/k_kg6jjxga/${searchInputValue}`)
+  fetch(`https://imdb-api.com/en/API/Search/k_bp53c8qv/${searchInputValue}`)
     .then((res) => res.json())
     .then((res) => {
+      if (!res.results) return;
       let firstItem = res.results[0];
       let movieId = firstItem.id;
       fetch(
-        `https://imdb-api.com/en/API/Title/k_kg6jjxga/${movieId}/FullActor,FullCast,Posters,Images,Trailer,Ratings,Wikipedia,`
+        `https://imdb-api.com/en/API/Title/k_bp53c8qv/${movieId}/FullActor,FullCast,Posters,Images,Trailer,Ratings,Wikipedia,`
       )
         .then((res) => res.json())
         .then((res) => {
@@ -21,22 +30,27 @@ searchIcon.addEventListener("click", () => {
             image: moviePoster,
           } = res;
           let movieDetailsElement = document.querySelector(".movie-details");
-
+          movieDetailsElement.innerHTML = "";
           let movieTitleDiv = document.createElement("div");
           let movieNameElement = document.createElement("h1");
           let movieDurationElement = document.createElement("p");
           movieTitleDiv.classList.add("movie-title");
           movieNameElement.classList.add("movie-name");
           movieDurationElement.classList.add("movie-duration");
-
           movieNameElement.innerText = movieTitle;
           movieDurationElement.innerText = movieDuration;
-
           movieTitleDiv.appendChild(movieNameElement);
           movieTitleDiv.appendChild(movieDurationElement);
-
           movieDetailsElement.appendChild(movieTitleDiv);
           //////////////////////////////
+          let movieDescriptionElement = document.createElement("p");
+          let movieReleaseElement = document.createElement("p");
+          movieDescriptionElement.classList.add("movie-description");
+          movieReleaseElement.classList.add("movie-release-year");
+          movieDescriptionElement.innerText = cutText(movieDescription);
+          movieReleaseElement.innerText = `Year:${releaseYear}`;
+          movieDetailsElement.appendChild(movieDescriptionElement);
+          movieDetailsElement.appendChild(movieReleaseElement);
         });
     });
 });
